e8ef164fce0ff716ab4f2f028bed3a9fbeb78281
Aditya Dharma <106744083+AdityaDharma@users.noreply.github.com>
2022-12-24 15:44:24 +0700

    Add files via upload

---
+++ TPMODUL3_ADITYA/Waiters.java
@@ -0,0 +1,46 @@
+package TPMODUL3_ADITYA;
+
+public class Waiters implements Runnable {
+
+    private final int orderQty;
+    private final int customerID;
+    static int foodPrice = 25000;
+
+    public Waiters(int customerID, int orderQty) {
+        this.customerID = customerID;
+        this.orderQty = orderQty;
+    }
+    @Override
+    public void run() {
+        while (true) {
+            getFood();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void orderInfo() {
+        System.out.println("==========================================================");
+        System.out.println("Customer ID: " + this.customerID);
+        System.out.println("Numbeer of Food: " + this.orderQty);
+        System.out.println("Total Price: " + this.orderQty * foodPrice);
+        System.out.println("==========================================================");
+    }
+    public void getFood() {
+        synchronized(Restaurant.getLock()) {
+
+            System.out.println("Waiter: Food is ready to deliver");
+            Restaurant restoran = new Restaurant();
+            restoran.setWaitingForPickup(false);
+
+            if (Restaurant.getfoodNumber() == this.orderQty + 1) {
+                orderInfo();
+                System.exit(0);
+            }
+            Restaurant.getLock().notifyAll();
+            System.out.println("Waiter: Tell the Restaurant to make another food\n");
+        }
+    }
+}
